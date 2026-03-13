f2b6eda5f11c37a888d8565631bf15676b1dbe7b
Valeria <valecorts6@gmail.com>
2026-03-13 16:32:49 -0600

    Arreglo de authController para obtener perfil

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getPublicKey } = require('../controllers/authController');
+const { register, login, getPublicKey, getMyProfile } = require('../controllers/authController');
 const verificarToken = require('../middlewares/authMiddleware');
 router.post('/register', register);
 router.post('/login', login);
